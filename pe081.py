b65d94c4c184dea42eb20201aa1ec8ad4f6452df
Viktar Arlou <varlou@gmail.com>
2015-11-22 20:39:36 +0100

    problem #81

---
+++ pe081.py
@@ -0,0 +1,36 @@
+"""
+Problem 81:
+
+Find the minimal path sum, in p081_matrix.txt file from the top left to the bottom right by only moving right and down.
+"""
+w = h = 80
+
+mtx = []
+f = open('problems/p081_matrix.txt')
+mtx = [list(map(int, line[:-1].rsplit(','))) for line in f]
+
+def memoize(f):
+    res={}
+    def lookup_f(*args):
+        rt = res.get(args,None)
+        if rt == None:
+            rt = f(*args)
+            res[args]=rt
+        return rt
+    return lookup_f
+    
+def find(x, y):
+    if x == w - 1 and y == h - 1:
+        return mtx[x][y]
+    rr = dd = 2**30
+    if x < w - 1:
+        rr = find(x + 1, y)
+    if y < h - 1:
+        dd = find(x, y + 1)
+    return mtx[x][y] + min([rr, dd])
+
+find = memoize(find)
+
+print(find(0, 0))
+    
+        
