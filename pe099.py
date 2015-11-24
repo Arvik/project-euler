c57c6222d266706988efe3c5adb1ec9e07224bd9
Viktar Arlou <varlou@gmail.com>
2015-11-24 18:38:16 +0100

    minor fix

--- pe099.py
+++ pe099.py
@@ -11,7 +11,7 @@ import math
 mtx = []
 f = open('problems/p099_base_exp.txt')
 mtx = [list(map(int, line[:-1].rsplit(','))) for line in f]
-rs = [m[1]*math.log10(m[0]) for m in mtx]
+rs = [m[1]*math.log(m[0]) for m in mtx]
 print(rs.index(max(rs)))
     
     
