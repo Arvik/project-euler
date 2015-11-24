4bb62b8af60e17bc8329a1a427b89efd4e2c048b
Viktar Arlou <varlou@gmail.com>
2015-11-24 18:35:32 +0100

    problem #99

---
+++ pe099.py
@@ -0,0 +1,17 @@
+"""
+
+a^x > b^y
+log(a^x) > log(b^y)
+x*log(a) > y(log(b)
+
+"""
+
+import math
+
+mtx = []
+f = open('problems/p099_base_exp.txt')
+mtx = [list(map(int, line[:-1].rsplit(','))) for line in f]
+rs = [m[1]*math.log10(m[0]) for m in mtx]
+print(rs.index(max(rs)))
+    
+    
