74e7edd30ef8b5b092d075592aa699a32d569661
Viktar Arlou <varlou@gmail.com>
2016-04-19 14:33:37 +0200

    problem 028

--- src/pe028.py
+++ src/pe028.py
@@ -1,9 +1,8 @@
-levels = 999 # 1001 -3
+width = 1001
 
-x = 1
-sum = x
-for i in range(2,levels+3, 2) :
+x, sum = 1, 1
+for i in range(2, width, 2) :
     for ii in range(4):
-        x+=i
+        x += i
         sum += x 
-print(sum)
+print(sum)  #669171001
